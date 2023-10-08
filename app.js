18f245f8a19f93db7ed8d5ec38336fb80440f3f7
AbtahiHasan <anikidpersonal@gmail.com>
2023-10-08 13:30:10 +0600

    setup page ui done

--- app.js
+++ app.js
@@ -21,10 +21,10 @@ try {
 }
 
 
+app.use(express.static(path.join(__dirname, "public")))
 app.set("view engine", "ejs")
 app.set("views", "./src/views")
 app.use(settingDone)
-app.use(express.static(path.join(__dirname, "public")))
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
