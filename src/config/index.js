8207e7a8a98627e1ae127ac5fdc4919536802398
AbtahiHasan <anikidpersonal@gmail.com>
2023-10-09 12:53:39 +0600

    nodemailer added

--- src/config/index.js
+++ src/config/index.js
@@ -9,5 +9,10 @@ module.exports = {
     port: process.env.PORT,
     database_url: process.env.DB_URL,
     session_secret: process.env.SESSION_SECRET,
+    smtp_host: process.env.SMTP_HOST,
+    smtp_port: process.env.SMTP_POST,
+    smtp_service: process.env.SMTP_SERVICE,
+    smtp_mail: process.env.SMTP_MAIL,
+    smtp_password: process.env.SMTP_PASSWORD,
 }
 
